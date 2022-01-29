fa84fc85cc19262961b29175857c5f31f0b69cb9
zmzade <zmahmoodzadeh@yahoo.com>
2022-01-29 17:54:19 +0100

    edited files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ const Navbar = () => {
         </nav>
         <a
           href="#contact"
-          className="mr-20 inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
+          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0"
         >
           Hire Me
           <ArrowRightIcon className="w-4 h-4 ml-1" />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,11 +7,11 @@ const Skills = () => {
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
-          <ChipIcon className="w-10 inline-block mb-4" />
+          <ChipIcon className="w-10 inline-block mb-4 text-gray-300" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills &amp; Technologies
           </h1>
-          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-200">
             I have experience working with following languages and technologies.
           </p>
         </div>
